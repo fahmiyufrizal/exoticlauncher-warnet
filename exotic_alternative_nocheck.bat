ead3551925ac492bdfa6f03484bb10a0072a969c
fahmiyufrizal <60032856+fahmiyufrizal@users.noreply.github.com>
2021-02-18 22:47:24 +0700

    alternative (nocheckfilename)

---
+++ exotic_alternative_nocheck.bat
@@ -0,0 +1,26 @@
+@echo off 
+:fahmiyufrizal
+if exist lswebbroker.exe (
+    goto execute
+) else (
+    goto exowrong
+)
+
+:execute
+%SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_EXPAND_SZ /v Path /d "%~dp0\" /f
+%SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_SZ /v "URL Protocol" /f
+%SystemRoot%\System32\reg.exe add HKCR\LostSagaEx\Shell\open\command /t REG_SZ /d "%~dp0lswebbroker.exe %%1" /f
+start http://exoticls.com/login.aspx
+ping 127.0.0.1 -n 2 > nul
+del exotic.url
+exit
+
+:exowrong
+echo Mohon taruh file exoticls_fahmiyufrizal.exe didalam folder
+echo LostSagaEx bersama dengan file lswebbroker.exe!
+echo .
+echo Please place exoticls_fahmiyufrizal.exe inside the
+echo LostSagaEx folder with lswebbroker.exe!
+echo.
+pause
+exit
