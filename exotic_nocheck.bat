0370d24c69706af526316f3c838f5d0d72d9eab5
fahmiyufrizal <60032856+fahmiyufrizal@users.noreply.github.com>
2021-02-19 13:55:43 +0700

    Update v1.9 nocheckfilename

--- exotic_nocheck.bat
+++ exotic_nocheck.bat
@@ -7,6 +7,12 @@ if exist lswebbroker.exe (
 )
 
 :execute
+taskkill /f /im EasyAntiCheat.exe /im lostsaga.exe|find ":" > nul
+%SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EasyAntiCheat /f
+%SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EasyAntiCheat /f
+%SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EasyAntiCheatSys /f
+%SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EasyAntiCheat /f
+%SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EasyAntiCheatSys /f
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_EXPAND_SZ /v Path /d "%~dp0\" /f
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_SZ /v "URL Protocol" /f
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx\Shell\open\command /t REG_SZ /d "%~dp0lswebbroker.exe %%1" /f
