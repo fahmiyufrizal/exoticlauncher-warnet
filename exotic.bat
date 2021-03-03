c6e0d536d243cdd3d1308d56b69268d5905f77ff
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2021-03-03 13:34:09 +0700

    Update 2.0.1

--- exotic.bat
+++ exotic.bat
@@ -1,4 +1,5 @@
 @echo off 
+
 :checkname
 if exist exoticls_fahmiyufrizal.exe (
     goto fahmiyufrizal
@@ -14,6 +15,7 @@ if exist lswebbroker.exe (
 )
 
 :execute
+start _configlauncher.exe
 taskkill /f /im EasyAntiCheat.exe /im lostsaga.exe|find ":" > nul
 %SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\EasyAntiCheat /f
 %SystemRoot%\System32\reg.exe delete HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\EasyAntiCheat /f
@@ -23,6 +25,17 @@ taskkill /f /im EasyAntiCheat.exe /im lostsaga.exe|find ":" > nul
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_EXPAND_SZ /v Path /d "%~dp0\" /f
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx /t REG_SZ /v "URL Protocol" /f
 %SystemRoot%\System32\reg.exe add HKCR\LostSagaEx\Shell\open\command /t REG_SZ /d "%~dp0lswebbroker.exe %%1" /f
+cd _configlauncher
+findstr /m "launch.exotic=url" setlauncher.ini
+if %errorlevel%==0 (
+    goto url ) else ( goto schdirect )
+
+:schdirect
+findstr /m "launch.exotic=direct" setlauncher.ini
+if %errorlevel%==0 (
+    goto direct ) else ( goto missconfig )
+
+:url
 echo [InternetShortcut] >"exotic.url"
 echo URL=http://exoticls.com/login.aspx >>"exotic.url"
 start exotic.url
@@ -30,21 +43,19 @@ ping 127.0.0.1 -n 2 > nul
 del exotic.url
 exit
 
+:direct
+start http://exoticls.com/login.aspx
+exit
+
 :exowrong
-echo Mohon taruh file exoticls_fahmiyufrizal.exe didalam folder
-echo LostSagaEx bersama dengan file lswebbroker.exe!
-echo .
-echo Please place exoticls_fahmiyufrizal.exe inside the
-echo LostSagaEx folder with lswebbroker.exe!
-echo.
-pause
+start exowrong.vbs
 exit
 
 :dontchange
-echo Jangan ubah nama filenya lur!
-echo Dont change the filename, bro!
-echo .
-echo fahmiyufrizal@2021
-echo.
-pause
+start dontchange.vbs
+exit
+
+:missconfig
+cd ..
+start missconfig.vbs
 exit
